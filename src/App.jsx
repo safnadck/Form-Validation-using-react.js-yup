05e46c4f84cf283601fa2af3dff358ef3ab0a7a2
muhammed safnad <muhammedsafnadck5@gmail.com>
2024-10-25 20:41:16 +0530

    25-10-2024

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormWithYup from "./Components/form-with-yup";
+import FormWithoutYup from "./Components/form-without-yup";
+
+function App() {
+  return (
+    <div>
+       <FormWithoutYup /> 
+     {/* <FormWithYup />  */}
+    </div>
+  );
+}
+
+export default App;
